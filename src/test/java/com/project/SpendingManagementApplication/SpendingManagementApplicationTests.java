8455b9b0ad4f7f2a45b48dd9faf65f1ebb005eca
Dat01 <thanhdatduong24102001@gmail.com>
2024-06-10 13:36:16 +0700

    commit local

---
+++ src/test/java/com/project/SpendingManagementApplication/SpendingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.SpendingManagementApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendingManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
