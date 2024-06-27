f30aea5771b2b421571a568a14286c1d4c5b4e4b
Dat02 <thanhdatduong24102001@gmail.com>
2024-06-27 21:23:43 +0700

    fix loi va update trang chu

--- src/main/java/com/project/SpendingManagementApplication/controller/AppController.java
+++ src/main/java/com/project/SpendingManagementApplication/controller/AppController.java
@@ -1,6 +1,7 @@
 package com.project.SpendingManagementApplication.controller;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +11,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import com.project.SpendingManagementApplication.entity.Khoanchi;
 import com.project.SpendingManagementApplication.entity.Khoanthu;
+import com.project.SpendingManagementApplication.entity.Tongtien;
 import com.project.SpendingManagementApplication.service.KhoanchiService;
 import com.project.SpendingManagementApplication.service.KhoanthuService;
+import com.project.SpendingManagementApplication.service.TongtienService;
+import com.project.SpendingManagementApplication.service.UserService;
 
 @Controller
 public class AppController {
@@ -19,15 +23,26 @@ public class AppController {
     KhoanchiService kcservice;
     @Autowired
     KhoanthuService ktservice;
+    @Autowired
+    TongtienService ttservice;
+    @Autowired
+    UserService uservice;
 
     @GetMapping("/")
     public String getAll(Model model){
         List<Khoanchi> ListKhoanchi = kcservice.getKhoanchi();
         List<Khoanthu> ListKhoanthu = ktservice.getKhoanthu();
         List<Object> combindedList = new ArrayList<>();
-        combindedList.add(ListKhoanchi);
-        combindedList.add(ListKhoanthu);
+        combindedList.addAll(ListKhoanchi);
+        combindedList.addAll(ListKhoanthu);
+        combindedList.sort((o1, o2) -> {
+            Date date1 = (o1 instanceof Khoanchi) ? ((Khoanchi) o1).getNgaychi() : ((Khoanthu) o1).getNgaythu();
+            Date date2 = (o2 instanceof Khoanchi) ? ((Khoanchi) o2).getNgaychi() : ((Khoanthu) o2).getNgaythu();
+            return date1.compareTo(date2);
+        });
         model.addAttribute("combindedList", combindedList);
+        Tongtien tt = ttservice.getTongtienbyID(uservice.getUserbyID(1).getIdtongtien().getIdtongtien());
+        model.addAttribute("tt", tt);
         return "homepage";
     }
 }
