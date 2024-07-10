989c567b237e026f2669b208b78b285f5d0ff886
Dat02 <thanhdatduong24102001@gmail.com>
2024-07-10 20:28:41 +0700

    update phan trang

--- src/main/java/com/project/SpendingManagementApplication/controller/AppController.java
+++ src/main/java/com/project/SpendingManagementApplication/controller/AppController.java
@@ -83,13 +83,13 @@ public class AppController {
         });
         
         int totalItems = (int) (pageKC.getTotalElements() + pageKT.getTotalElements());
-        int start = (pageNo-1)*pageSize;
-        int end = Math.min(start+pageSize, totalItems);
+        int total = combindedList.size();
+        int start = Math.min((pageNo - 1) * pageSize, totalItems);
+        int end = Math.min(start + pageSize, totalItems);
         List<Object> paginatedList=combindedList.subList(start, end);
 
         model.addAttribute("currentPage", pageNo);
-        model.addAttribute("totalPages", (int) Math.ceil((double) totalItems/pageSize));
-        // model.addAttribute("totalPages", Math.max(pageKC.getTotalPages(), pageKT.getTotalPages()));
+        model.addAttribute("totalPages", (int) Math.ceil((double) total/pageSize));
         model.addAttribute("totalItems", totalItems);
 
         model.addAttribute("combindedList", paginatedList);
